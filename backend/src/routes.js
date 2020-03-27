da0344fe9f4d81d3ea9b77fd37d31717bcae0ace
root <lukasvicente>
2020-03-27 01:15:50 -0300

    27-03-20 - create e list ongs

---
+++ backend/src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const connection = require('./database/connection');
+
+const OngController = require('./controllers/OngController');
+
+const routes = express.Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+module.exports = routes;
